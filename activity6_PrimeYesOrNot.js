82f49c9e42023e373d424bda598c18f6a96ee5b3
OsorioE <106990741+osorio99@users.noreply.github.com>
2024-04-29 12:54:13 -0500

    Update activity6_PrimeYesOrNot.js

--- activity6_PrimeYesOrNot.js
+++ activity6_PrimeYesOrNot.js
@@ -1,29 +1,10 @@
-function Prime(number) {
+function cousin(number) {
+    if(number>1){
+        if(number%2 ==0){
+            return "no es primo"
+        }else return "es primo"
 
-    if (number <= 1) {
-       return false
-    } else if (number <= 3) {
-        return  true
-    } else if (number % 2 == 0 || number % 3 == 0) {
-        return false
-    }
-
-    for (let i = 5; i * i <= number; i += 6) {
-        if (number % i == 0 || number % (i + 2) == 0) {
-           return false
-        }
-    }
-    return true
+    }else return "valide bien el numero"
 }
-
-
-function check(){
-    let number = 100
-    if (Prime(number)) {
-        console.log(number +' es primo');
-    } else {
-        console.log(number +' no es primo');
-}
-}
-
-check()
+exit= cousin(97)
+console.log(exit)
